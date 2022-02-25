bf994e970c1a20f9608e82f3ff24e444c18796f2
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-02-25 18:25:27 +0100

    printed cards using foreach loop

--- index.php
+++ index.php
@@ -21,20 +21,25 @@ class Movie {
 
     public function getMovieInfos() {
         return
-        "<h2 class='py-1'> $this->title </h2>" . "<br>" .
-        "<div><b> YEAR: </b> $this->year </div>" . "<br>" . 
-        "<div><b> GENRE: </b> $this->genre  </div>" . "<br>" . 
-        "<h5 class='pt-2' my-2'>  PLOT </h5>" .
-        "<p> $this->plot </p>";
+        "<div class='card col-5 p-5 mx-auto my-2 d-flex justify-content-around bg-light'>           
+            <h2 class='py-1'> $this->title </h2> 
+            <div><b> YEAR: </b> $this->year </div> 
+            <div><b> GENRE: </b> $this->genre </div> 
+            <h5 class='pt-2' my-2'> PLOT </h5> 
+            <p> $this->plot </p>
+        </div>"
+        ;
     }
 }
+$movies = [
+    new Movie("V for Vendetta", "2006", "Action, Mystery, Thriller", "Following world war, London is a police state occupied by a fascist government, and a vigilante known only as V (Hugo Weaving) uses terrorist tactics to fight the oppressors of the world in which he now lives. When V saves a young woman named Evey (Natalie Portman) from the secret police, he discovers an ally in his fight against England's oppressors."),
+    new Movie("The Green Mile", "1999", "Drama, Fantasy", "Paul Edgecomb (Tom Hanks) walked the mile with a variety of cons. He had never encountered someone like John Coffey (Michael Clarke Duncan), a massive black man convicted of brutally killing a pair of young sisters. Coffey had the size and strength to kill anyone, but not the demeanor. Beyond his simple, naive nature and a deathly fear of the dark, Coffey seemed to possess a prodigious, supernatural gift. Paul began to question whether Coffey was truly guilty of murdering the two girls. "),
+    new Movie("Trainspotting", "2004", "Drama, Comedy", "Heroin addict Mark Renton (Ewan McGregor) stumbles through bad ideas and sobriety attempts with his unreliable friends -- Sick Boy (Jonny Lee Miller), Begbie (Robert Carlyle), Spud (Ewen Bremner) and Tommy (Kevin McKidd). He also has an underage girlfriend, Diane (Kelly Macdonald), along for the ride. After cleaning up and moving from Edinburgh to London, Mark finds he can't escape the life he left behind when Begbie shows up at his front door on the lam, and a scheming Sick Boy follows"),
+    new Movie("Pulp Fiction", "2002", "Crime, Drama", "Vincent Vega (John Travolta) and Jules Winnfield (Samuel L. Jackson) are hitmen with a penchant for philosophical discussions. In this ultra-hip, multi-strand crime movie, their storyline is interwoven with those of their boss, gangster Marsellus Wallace (Ving Rhames); his actress wife, Mia (Uma Thurman); struggling boxer Butch Coolidge (Bruce Willis) ; master fixer Winston Wolfe (Harvey Keitel) and a nervous pair of armed robbers, 'Pumpkin' (Tim Roth) and 'Honey Bunny' (Amanda Plummer)."),
+    new Movie("Fight Club", "1999", "Drama, Mystery, Thriller", "A depressed man (Edward Norton) suffering from insomnia meets a strange soap salesman named Tyler Durden (Brad Pitt) and soon finds himself living in his squalid house after his perfect apartment is destroyed. The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives. Their perfect partnership frays when Marla (Helena Bonham Carter), a fellow support group crasher, attracts Tyler's attention."),
+    new Movie("Full Metal Jacket", "2001", "War, Drama, History", "Stanley Kubrick's take on the Vietnam War follows smart-aleck Private Davis (Matthew Modine), quickly christened 'Joker' by his foul-mouthed drill sergeant (R. Lee Ermey), and pudgy Private Lawrence (Vincent D'Onofrio), nicknamed 'Gomer Pyle', as they endure the rigors of basic training. Though Pyle takes a frightening detour, Joker graduates to the Marine Corps and is sent to Vietnam as a journalist, covering -- and eventually participating in -- the bloody Battle of Hué."),
+];
 
-$vForVendetta = new Movie("V for Vendetta", "2006", "Action, Mystery, Thriller", "Following world war, London is a police state occupied by a fascist government, and a vigilante known only as V (Hugo Weaving) uses terrorist tactics to fight the oppressors of the world in which he now lives. When V saves a young woman named Evey (Natalie Portman) from the secret police, he discovers an ally in his fight against England's oppressors.");
-$theGreenMile = new Movie("The Green Mile", "1999", "Drama, Fantasy", "Paul Edgecomb (Tom Hanks) walked the mile with a variety of cons. He had never encountered someone like John Coffey (Michael Clarke Duncan), a massive black man convicted of brutally killing a pair of young sisters. Coffey had the size and strength to kill anyone, but not the demeanor. Beyond his simple, naive nature and a deathly fear of the dark, Coffey seemed to possess a prodigious, supernatural gift. Paul began to question whether Coffey was truly guilty of murdering the two girls. ");
-$trainspotting = new Movie("Trainspotting", "2004", "Drama, Comedy", "Heroin addict Mark Renton (Ewan McGregor) stumbles through bad ideas and sobriety attempts with his unreliable friends -- Sick Boy (Jonny Lee Miller), Begbie (Robert Carlyle), Spud (Ewen Bremner) and Tommy (Kevin McKidd). He also has an underage girlfriend, Diane (Kelly Macdonald), along for the ride. After cleaning up and moving from Edinburgh to London, Mark finds he can't escape the life he left behind when Begbie shows up at his front door on the lam, and a scheming Sick Boy follows");
-$pulpFiction = new Movie("Pulp Fiction", "2002", "Crime, Drama", "Vincent Vega (John Travolta) and Jules Winnfield (Samuel L. Jackson) are hitmen with a penchant for philosophical discussions. In this ultra-hip, multi-strand crime movie, their storyline is interwoven with those of their boss, gangster Marsellus Wallace (Ving Rhames); his actress wife, Mia (Uma Thurman); struggling boxer Butch Coolidge (Bruce Willis) ; master fixer Winston Wolfe (Harvey Keitel) and a nervous pair of armed robbers, 'Pumpkin' (Tim Roth) and 'Honey Bunny' (Amanda Plummer).");
-$fightClub = new Movie("Fight Club", "1999", "Drama, Mystery, Thriller", "A depressed man (Edward Norton) suffering from insomnia meets a strange soap salesman named Tyler Durden (Brad Pitt) and soon finds himself living in his squalid house after his perfect apartment is destroyed. The two bored men form an underground club with strict rules and fight other men who are fed up with their mundane lives. Their perfect partnership frays when Marla (Helena Bonham Carter), a fellow support group crasher, attracts Tyler's attention.");
-$fullMetalJacket = new Movie("Full Metal Jacket", "2001", "War, Drama, History", "Stanley Kubrick's take on the Vietnam War follows smart-aleck Private Davis (Matthew Modine), quickly christened 'Joker' by his foul-mouthed drill sergeant (R. Lee Ermey), and pudgy Private Lawrence (Vincent D'Onofrio), nicknamed 'Gomer Pyle', as they endure the rigors of basic training. Though Pyle takes a frightening detour, Joker graduates to the Marine Corps and is sent to Vietnam as a journalist, covering -- and eventually participating in -- the bloody Battle of Hué.");
 
 ?>
 
@@ -50,46 +55,17 @@ $fullMetalJacket = new Movie("Full Metal Jacket", "2001", "War, Drama, History",
 </head>
 <body class="bg-danger">
 
-    <div class="container py-5 mx-auto">
-       <h1 class="text-light">MOVIES CARDS</h1>
-        <div class="cards-container py-5 d-flex flex-wrap justify-content-between">
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-                <div class="movie-infos p-3">
-                    <?php echo $vForVendetta->getMovieInfos(); ?>
-                </div>
-            </div>
-
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-                <div class="movie-infos p-3">
-                    <?php echo $theGreenMile->getMovieInfos(); ?>
-                </div>    
-            </div>
-
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-            <div class="movie-infos p-3">
-                    <?php echo $trainspotting->getMovieInfos(); ?>
-                </div>
-            </div>
-
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-                <div class="movie-infos p-3">
-                    <?php echo $pulpFiction->getMovieInfos(); ?>
-                </div>
-            </div>
-
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-                <div class="movie-infos p-3">
-                    <?php echo $fightClub->getMovieInfos(); ?>
-                </div>
-            </div>
-
-            <div class="card col-4 my-2 p-4 bg-light border-danger">
-                <div class="movie-infos p-3">
-                    <?php echo $fullMetalJacket->getMovieInfos(); ?>
-                </div>
-            </div>
+    <div class="container mx-auto">
+       <h1 class="text-light p-5">MOVIES CARDS</h1>
+        <div class="cards-container d-flex flex-wrap justify-content-beween">
+            <?php
+                foreach ($movies as $movie) { 
+                    echo $movie->getMovieInfos();
+                }
+            ?>
         </div>
     </div>
     
 </body>
 </html>
+
